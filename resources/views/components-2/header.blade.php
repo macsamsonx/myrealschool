c7737e074eeb58e353d324f3e5f0656692867d2f
macsamsonx <mark.angelo.t.samson@gmail.com>
2023-08-07 23:15:40 +0800

    MyRealSchool Project

---
+++ resources/views/components-2/header.blade.php
@@ -0,0 +1,43 @@
+<header class="bg-white shadow">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+            <a href="{{ route('home') }}" class="">
+                <img class="h-20 w-auto" src="{{ asset('asset/logo.png') }}" alt="">
+            </a>
+        </div>
+        <div class="flex lg:hidden">
+            <button type="button"
+                class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                <span class="sr-only">Open main menu</span>
+                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12">
+            @php
+                $routes = ['home', 'pricing', 'why-us', 'carrer-hub', 'contact-us'];
+            @endphp
+            @foreach ($routes as $route)
+                <div class="px-1 py-1">
+                    <a href="{{ route($route) }}"
+                        class="text-lg leading-6  {{ request()->routeIs($route) ? 'font-bold  text-teal-900' : ' text-black' }}">{{ ucwords(str_replace('-', ' ', $route)) }}</a>
+                    @if (request()->routeIs($route))
+                        <div class="w-auto h-1 bg-teal-900 mt-4"></div>
+                    @endif
+
+                </div>
+            @endforeach
+            {{-- Text Size : text-sm text-md text-lg  --}}
+            <div
+                class="w-32 px-5 py-1 rounded-3xl border border-teal-900 justify-center items-center inline-flex">
+                <a href="{{ route('login') }}" class="text-teal-900 text-lg font-bold">LOGIN</a>
+            </div>
+
+        </div>
+    </nav>
+    <!-- Mobile menu, show/hide based on menu open state. -->
+
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,130 @@
+<x-guest-layout>
+    {{-- First --}}
+    <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+        <img src="{{ asset('asset/banner-1.png') }}" alt=""
+            class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <div class="w-96 text-zinc-600 text-base font-bold leading-relaxed tracking-widest">LAUNCH YOUR REAL
+                    ESTATE JOURNEY</div>
+                <h2 class="text-4xl font-bold text-zinc-900 sm:text-6xl mt-3">Enroll Today</h2>
+                <h2 class="text-4xl font-bold text-zinc-900 sm:text-6xl mt-3">for <span
+                        class="text-amber-400 text-8xl font-bold leading-10">Success!</span></h2>
+                <p class="w-96 text-zinc-600 text-lg font-normal leading-relaxed">Anim aute id magna aliqua ad ad non
+                    deserunt sunt. Qui
+                    irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                <div class="w-96 text-zinc-900 text-lg font-bold leading-relaxed">Choose your state to get started:
+                </div>
+                <form class="items-center justify-between mt-4" action="" method="POST">
+
+                    <select name="" id="" class="w-80 py-3 rounded-3xl border border-teal-900">
+                        <option value="">Choose your State</option>
+                        <option value="">Baliuag</option>
+                    </select>
+                    <button type="submit" class="w-48 px-10 py-3 bg-teal-900 rounded-3xl text-white">GET
+                        STARTED</button>
+                    {{-- <div class="w-48 h-16 px-10 py-5 bg-teal-900 rounded-3xl border border-teal-900 justify-center items-center gap-2.5 inline-flex">
+                        <div class="text-white text-lg font-bold">GET STARTED</div>
+                      </div> --}}
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- Second --}}
+    <div class="relative isolate overflow-hidden  py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mt-8 grid grid-cols-1 gap-4 sm:mt-20 sm:grid-cols-2 lg:grid-cols-2">
+                <div class="flex flex-col-reverse">
+                    <div class="mx-auto max-w-2xl lg:mx-0">
+                        <img class="w-96 h-96" src="{{ asset('asset/image/Group 53 1.png') }}" />
+                    </div>
+                </div>
+                <div class="flex flex-col-reverse">
+                    <div class="mx-auto max-w-2xl lg:mx-0">
+                        <div class="w-96 text-zinc-600 text-base font-bold uppercase leading-relaxed tracking-wider">why
+                            choose us</div>
+                        <div class="w-96"><span style="text-zinc-900 text-5xl font-bold leading-10">Unlock Success
+                                With<br />My</span><span
+                                style="text-teal-900 text-5xl font-bold leading-10">Real</span><span
+                                style="text-lime-400 text-5xl font-bold leading-10">School</span><span
+                                style="text-zinc-900 text-5xl font-bold leading-10">: </span><span
+                                style="text-amber-300 text-5xl font-bold leading-10">Your<br />Path to Excellence</span>
+                        </div>
+                        <div class="w-96 text-zinc-600 text-lg font-normal leading-relaxed">Lorem ipsum dolor sit amet,
+                            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                            magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores</div>
+                        <div class="text-black text-lg font-bold leading-relaxed">Expert Trainers</div>
+                        <div class="w-8 h-8 relative flex-col justify-start items-start inline-flex">
+                            <div class="w-8 h-8 relative">
+                            </div>
+                        </div>
+                        <div class="text-black text-lg font-bold leading-relaxed">Online Learning</div>
+                        <div class="w-8 h-8 relative flex-col justify-start items-start inline-flex">
+                            <div class="w-8 h-8 relative">
+                            </div>
+                        </div>
+                        <div class="text-black text-lg font-bold leading-relaxed">Fully Approved By State</div>
+                        <div class="w-8 h-8 relative flex-col justify-start items-start inline-flex">
+                            <div class="w-8 h-8 relative">
+                            </div>
+                        </div>
+                        <div class="text-black text-lg font-bold leading-relaxed">Lifetime Access</div>
+                        <div class="w-8 h-8 relative flex-col justify-start items-start inline-flex">
+                            <div class="w-8 h-8 relative">
+                            </div>
+                        </div>
+                        <div
+                            class="w-48 h-16 px-10 py-5 bg-teal-900 rounded-3xl border border-teal-900 justify-center items-center gap-2.5 inline-flex">
+                            <div class="text-white text-lg font-bold">GET STARTED</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        {{-- <div class="items-center justify-between px-4">
+            <div class="mx-auto">
+                <img class="w-96 h-96" src="{{ asset('asset/image/Group 53 1.png') }}" />
+            </div>
+            <div class="content">
+                <div class="w-96 text-zinc-600 text-base font-bold uppercase leading-relaxed tracking-wider">why choose us</div>
+            </div>
+
+        </div> --}}
+        {{-- <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
+            <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+          </div>
+          <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+              <a href="#">Open roles <span aria-hidden="true">&rarr;</span></a>
+              <a href="#">Internship program <span aria-hidden="true">&rarr;</span></a>
+              <a href="#">Our values <span aria-hidden="true">&rarr;</span></a>
+              <a href="#">Meet our leadership <span aria-hidden="true">&rarr;</span></a>
+            </div>
+            <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+              <div class="flex flex-col-reverse">
+                <dt class="text-base leading-7 text-gray-300">Offices worldwide</dt>
+                <dd class="text-2xl font-bold leading-9 tracking-tight text-white">12</dd>
+              </div>
+              <div class="flex flex-col-reverse">
+                <dt class="text-base leading-7 text-gray-300">Full-time colleagues</dt>
+                <dd class="text-2xl font-bold leading-9 tracking-tight text-white">300+</dd>
+              </div>
+              <div class="flex flex-col-reverse">
+                <dt class="text-base leading-7 text-gray-300">Hours per week</dt>
+                <dd class="text-2xl font-bold leading-9 tracking-tight text-white">40</dd>
+              </div>
+              <div class="flex flex-col-reverse">
+                <dt class="text-base leading-7 text-gray-300">Paid time off</dt>
+                <dd class="text-2xl font-bold leading-9 tracking-tight text-white">Unlimited</dd>
+              </div>
+            </dl>
+          </div>
+        </div> --}}
+    </div>
+</x-guest-layout>
